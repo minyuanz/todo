d83f79aff0f554656aaceb172813dc748828eb94
minyuan <a1097562@gmail.com>
2023-10-31 15:47:44 +0800

    'url'

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import { BootstrapVueNextResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/todolist/',
+  base: '/todo/',
   plugins: [
     vue(),
     Components({
